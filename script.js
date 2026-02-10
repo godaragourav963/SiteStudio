364fdac27c68b54ed3b88f009e2a3ed4c9a64d3a
godaragourav963 <godaragourav963@gmail.com>
2026-02-10 21:02:09 +0530

    day2

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const propertiesBox = document.getElementById("properties");
+const noSelection = document.getElementById("no-selection");
+
+const textInput = document.getElementById("prop-text");
+const fontSizeInput = document.getElementById("prop-font-size");
+const colorInput = document.getElementById("prop-color");
+const paddingInput = document.getElementById("prop-padding");
+
 const canvas = document.getElementById("canvas");
 
 let selectedElement = null;
@@ -40,7 +48,6 @@ function addToCanvas(el) {
 
 
 function selectElement(el) {
-  // Remove selection from previous
   if (selectedElement) {
     selectedElement.classList.remove("selected");
   }
@@ -48,12 +55,62 @@ function selectElement(el) {
   selectedElement = el;
   selectedElement.classList.add("selected");
 
-  console.log("Selected:", el.tagName);
+  noSelection.style.display = "none";
+  propertiesBox.style.display = "block";
+
+  // Fill inputs from element
+  textInput.value = el.textContent || "";
+  fontSizeInput.value = parseInt(getComputedStyle(el).fontSize) || "";
+  colorInput.value = rgbToHex(getComputedStyle(el).color);
+  paddingInput.value = parseInt(getComputedStyle(el).padding) || "";
 }
+
 
 canvas.addEventListener("click", () => {
   if (selectedElement) {
     selectedElement.classList.remove("selected");
     selectedElement = null;
+
+    propertiesBox.style.display = "none";
+    noSelection.style.display = "block";
   }
 });
+
+
+textInput.addEventListener("input", () => {
+  if (selectedElement) {
+    selectedElement.textContent = textInput.value;
+  }
+});
+
+fontSizeInput.addEventListener("input", () => {
+  if (selectedElement) {
+    selectedElement.style.fontSize = fontSizeInput.value + "px";
+  }
+});
+
+colorInput.addEventListener("input", () => {
+  if (selectedElement) {
+    selectedElement.style.color = colorInput.value;
+  }
+});
+
+paddingInput.addEventListener("input", () => {
+  if (selectedElement) {
+    selectedElement.style.padding = paddingInput.value + "px";
+  }
+});
+
+
+
+function rgbToHex(rgb) {
+  const result = rgb.match(/\d+/g);
+  if (!result) return "#000000";
+
+  return (
+    "#" +
+    result
+      .map(x => parseInt(x).toString(16).padStart(2, "0"))
+      .join("")
+  );
+}
